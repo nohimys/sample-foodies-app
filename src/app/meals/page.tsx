6e9f272bde7903dbe3ce1bb1d894d4179138cdae
nohimgodage <nohim.godage@gmail.com>
2023-12-31 00:37:13 +0530

    Using React Suspense for granular level loading

--- src/app/meals/page.tsx
+++ src/app/meals/page.tsx
@@ -3,10 +3,18 @@ import Link from 'next/link';
 import classes from './page.module.css';
 import MealsGrid from '@/components/meals/meals-grid';
 import {getMeals} from "@/lib/meals";
+import {Suspense} from "react";
+import MealsLoadingPage from "@/app/meals/loading-not-used";
 
-const MealsPage = async () => {
+const Meals = async () => {
     const meals = await getMeals();
 
+    return (
+        <MealsGrid meals={meals} />
+    );
+}
+
+const MealsPage = () => {
     return (
         <>
             <header className={classes.header}>
@@ -24,7 +32,9 @@ const MealsPage = async () => {
                 </p>
             </header>
             <main className={classes.main}>
-                <MealsGrid meals={meals} />
+                <Suspense fallback={<MealsLoadingPage/>}>
+                    <Meals/>
+                </Suspense>
             </main>
         </>
     );
