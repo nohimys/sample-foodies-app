d17de1f36e5a392b2a28697b120498d5ea675e11
nohimgodage <nohim.godage@gmail.com>
2023-12-31 12:49:32 +0530

    Fetch single meal from database & render it on the single meal page.

--- src/app/meals/[slug]/page.tsx
+++ src/app/meals/[slug]/page.tsx
@@ -1,9 +1,35 @@
-const MealDetailsPage = ({params}: any) => {
+import Image from 'next/image';
+
+import { getMeal } from '@/lib/meals';
+import classes from './page.module.css';
+
+export default function MealDetailsPage({ params }: any) {
+    const meal = getMeal(params.slug);
+
+    meal.instructions = meal.instructions.replace(/\n/g, '<br />');
+
     return (
         <>
-            <h1>Meal Details Page</h1>
-            <h2>Param: {params && params.slug}</h2>
+            <header className={classes.header}>
+                <div className={classes.image}>
+                    <Image src={meal.image} alt={meal.title} fill />
+                </div>
+                <div className={classes.headerText}>
+                    <h1>{meal.title}</h1>
+                    <p className={classes.creator}>
+                        by <a href={`mailto:${meal.creator_email}`}>{meal.creator}</a>
+                    </p>
+                    <p className={classes.summary}>{meal.summary}</p>
+                </div>
+            </header>
+            <main>
+                <p
+                    className={classes.instructions}
+                    dangerouslySetInnerHTML={{
+                        __html: meal.instructions,
+                    }}
+                ></p>
+            </main>
         </>
     );
 }
-export default MealDetailsPage;
