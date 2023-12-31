1c79b640d1a05598f6db918855a2e6fde6fed83b
nohimgodage <nohim.godage@gmail.com>
2023-12-31 19:27:23 +0530

    Using 'useFormStatus' Hook for better UX.

--- src/app/meals/share/page.tsx
+++ src/app/meals/share/page.tsx
@@ -1,8 +1,7 @@
-'use client'
-
 import classes from './page.module.css';
 import ImagePicker from "@/components/meals/image-picker";
 import {shareMeal} from "@/lib/actions";
+import MealsFormSubmit from "@/components/meals/meals-form-submit";
 
 export default function ShareMealPage() {
 
@@ -45,7 +44,7 @@ export default function ShareMealPage() {
                     </p>
                     <ImagePicker label={'Your image'} name={'image'}/>
                     <p className={classes.actions}>
-                        <button type="submit">Share Meal</button>
+                        <MealsFormSubmit/>
                     </p>
                 </form>
             </main>
